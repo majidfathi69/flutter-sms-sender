772e94631a8e02232066b9873e151cb09c04139d
“Majid <majidfathi69@gmail.com>
2023-08-22 16:48:42 +0330

    sms sending details

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'dart:io';
-import 'package:path/path.dart';
 import 'package:excel/excel.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,9 +23,27 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   static const platform = MethodChannel('sendSms');
 
-  readXLSXFile() {
-    var file = '/Users/majid/Projects/sen_sms/lib/assets/test.xlsx';
-    var bytes = File(file).readAsBytesSync();
+  final total = 0.obs;
+  final count = 0.obs;
+  final cPhone = 'cPhone'.obs;
+  final cMsg = 'cMsg'.obs;
+  bool stop = false;
+  final delayed = 0.obs;
+
+  uploadXlsxFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      File file = File(result.files.single.path!);
+      readXLSXFile(file);
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  readXLSXFile(File file) async {
+    // var fileAddr = '/Users/majid/Projects/sen_sms/lib/assets/test.xlsx';
+    // var bytes = File(fileAddr).readAsBytesSync();
+    var bytes = file.readAsBytesSync();
     var excel = Excel.decodeBytes(bytes);
 
     for (var table in excel.tables.keys) {
@@ -32,30 +53,67 @@ class _MainAppState extends State<MainApp> {
         print(excel.tables[table]?.maxCols);
         print(excel.tables[table]?.maxRows);
       }
+
+      total.value = excel.tables[table]!.maxRows;
       for (var row in excel.tables[table]!.rows) {
         if (kDebugMode) {
           print('${row[0]!.value}');
           print(row[1]!.value.toString());
           print('${row[2]!.value}');
         }
+        String phone = row[0]!.value.toString();
+        String msg = row[1]!.value.toString();
+        int delay = row[2]!.value;
+
+        cMsg.value = msg;
+        cPhone.value = phone;
+        if (stop) {
+          stop = false;
+          return;
+        }
+        sendSms(phone, msg);
+        var counter = delay;
+
+        Timer.periodic(const Duration(seconds: 1), (timer) {
+          if (kDebugMode) {
+            print(timer.tick);
+          }
+          counter--;
+          delayed.value = counter;
+          if (counter == 0) {
+            if (kDebugMode) {
+              print('Cancel timer');
+            }
+            timer.cancel();
+          }
+        });
+        await Future.delayed(Duration(seconds: delay));
       }
     }
   }
 
-  Future<void> sendSms() async {
+  Future<void> sendSms(String phone, String msg) async {
     if (kDebugMode) {
       print("SendSMS");
     }
     try {
-      final String result = await platform.invokeMethod(
-          'send', <String, dynamic>{
-        "phone": "09199795867",
-        "msg": "Hello! I'm sent programatically."
-      }); //Replace a 'X' with 10 digit phone number
+      final String result =
+          await platform.invokeMethod('send', <String, dynamic>{
+        "phone": phone,
+        "msg": msg,
+      });
+      count.value += 1;
       if (kDebugMode) {
         print(result);
       }
     } on PlatformException catch (e) {
+      Get.snackbar(
+        'error',
+        e.toString(),
+        colorText: Colors.red,
+        backgroundColor: Colors.lightBlue,
+        icon: const Icon(Icons.add_alert),
+      );
       if (kDebugMode) {
         print(e.toString());
       }
@@ -71,14 +129,51 @@ class _MainAppState extends State<MainApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () => readXLSXFile(),
-                child: const Text('test xlsx file'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      total.value = 0;
+                      count.value = 0;
+                      uploadXlsxFile();
+                    },
+                    child: const Text('upload xlsx file'),
+                  ),
+                  const SizedBox(width: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      stop = true;
+                    },
+                    child: const Text('Stop'),
+                  ),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () => sendSms(),
-                child: const Text("Send SMS"),
-              )
+              const SizedBox(height: 20),
+              Obx(() => Text('${count.value} / ${total.value}')),
+              const SizedBox(height: 20),
+              Obx(() => Text('next: ${delayed.value}')),
+              const SizedBox(height: 20),
+              Obx(
+                () => Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text('Current: '),
+                        Text(cPhone.value),
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 50,
+                      child: Text(
+                        cMsg.value,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
